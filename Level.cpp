70dcf55b2e7249834d0aa76ed1651e2989d4949e
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-12-12 16:40:26 +0000

    Started diamond setup

--- Level.cpp
+++ Level.cpp
@@ -293,3 +293,45 @@ std::vector< GridObject* > Level::GetObjectAt(sf::Vector2i _targetPos)
 	// (default constructor)
 	return std::vector<GridObject*>();
 }
+
+bool Level::RemoveObject(GridObject* _toMove, sf::Vector2i _targetPos)
+{
+	// Don't trust other code
+// Make sure _toMove is a valid pointer
+// Also check our target position is within the grid
+	if (_toMove != nullptr
+		&& _targetPos.y >= 0 && _targetPos.y < m_contents.size()
+		&& _targetPos.x >= 0 && _targetPos.x < m_contents[_targetPos.y].size())
+	{
+		// Get the current position of the grid object
+		sf::Vector2i oldPos = _toMove->getGridPosition();
+
+		// Find the object in the list using an iterator 
+		// and the find method
+		auto it = std::find(m_contents[oldPos.y][oldPos.x].begin(),
+			m_contents[oldPos.y][oldPos.x].end(),
+			_toMove);
+
+		// If we found the object at this location,
+		// it will NOT equal the end of the vector
+		if (it != m_contents[oldPos.y][oldPos.x].end())
+		{
+			// We found the object!
+
+			// Remove it from the old position
+			m_contents[oldPos.y][oldPos.x].erase(it);
+
+			// Add it to its new position
+			//m_contents[_targetPos.y][_targetPos.x].push_back(_toMove);
+
+			// Tell the object it's new position
+			//_toMove->SetGridPosition(_targetPos);
+
+			// Return success
+			return true;
+		}
+	}
+
+	// return failure
+	return false;
+}
